642344b59f5f698ed0faeaa257e9bb5a15f8df33
ZhangZisu <admin@zhangzisu.cn>
2018-10-09 11:23:04 +0800

    Update version

--- .electron-vue/dev-runner.js
+++ .electron-vue/dev-runner.js
@@ -161,14 +161,13 @@ function greeting () {
   const cols = process.stdout.columns
   let text = ''
 
-  if (cols > 104) text = 'electron-vue'
-  else if (cols > 76) text = 'electron-|vue'
+  if (cols > 76) text = 'electron|vue'
   else text = false
 
   if (text) {
     say(text, {
       colors: ['yellow'],
-      font: 'simple3d',
+      font: 'block',
       space: false
     })
   } else console.log(chalk.yellow.bold('\n  electron-vue'))
